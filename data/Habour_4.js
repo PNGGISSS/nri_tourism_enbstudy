e5df0c37f53a97b530f4f7f5000c4c979f2dadc9
PNG GIS & Survey Solutions Limited <png.gisss@outlook.com>
2025-08-07 15:42:52 +1000

    Add files via upload

---
+++ data/Habour_4.js
@@ -0,0 +1,1 @@
+var json_Habour_4 = {"type":"FeatureCollection","name":"Habour_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Name":"Simpson Habour"},"geometry":{"type":"Point","coordinates":[152.166204017619606,-4.201723023198369]}}]}
